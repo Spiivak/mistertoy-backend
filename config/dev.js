8608eba801228f0165f4d11e30f0501171640f00
Spiivak <edenspiivak@gmail.com>
2024-01-01 09:11:23 +0200

    attemp to work with mongo atlas

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 export default {
-    dbURL: 'mongodb+srv://spiivak:eden9216240@mistertoy.tgq0ngm.mongodb.net/',
+    dbURL: 'mongodb+srv://spiivak:eden9216240@mistertoy.tgq0ngm.mongodb.net/?retryWrites=true&w=majority',
     dbName: 'toy_db',
 }
 // export default {
